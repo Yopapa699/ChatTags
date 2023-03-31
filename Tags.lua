72f0e28f0402f135d965c75434322af5f5227286
Yxu_Sin <104718666+Yopapa699@users.noreply.github.com>
2023-03-31 15:12:25 +0800

    Update Tags.lua

--- Tags.lua
+++ Tags.lua
@@ -1,9 +1,8 @@
 return {
     ["RifqiKil"] = {
-        TagText = "HUB OWNER",
+        TagText = "VAPE OWNER",
         TagColor = Color3.fromRGB(42, 242, 218)
     },
-
     ["ashadif937"] = {
         TagText = "CO OWNER",
         TagColor = Color3.fromRGB(199, 135, 17)
