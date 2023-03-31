73608293aa6d3da7d74977076a80112f7054b99e
Yxu_Sin <104718666+Yopapa699@users.noreply.github.com>
2023-03-31 14:37:31 +0800

    Create Tags.lua

---
+++ Tags.lua
@@ -0,0 +1,11 @@
+return {
+    ["RifqiKil"] = {
+        TagText = "HUB OWNER",
+        TagColor = Color3.fromRGB(42, 242, 218)
+    },
+
+    ["ashadif937"] = {
+        TagText = "CO OWNER",
+        TagColor = Color3.fromRGB(199, 135, 17)
+    }
+}
