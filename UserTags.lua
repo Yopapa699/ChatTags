7d6ff1ef87ba3319c92c41a1003365ffc258eda7
Yxu_Sin <104718666+Yopapa699@users.noreply.github.com>
2023-05-14 14:41:12 +0800

    Update and rename Tags.lua to UserTags.lua

--- UserTags.lua
+++ UserTags.lua
@@ -3,8 +3,4 @@ return {
         TagText = "VAPE OWNER",
         TagColor = Color3.fromRGB(231, 76, 60)
     },
-    ["ashadif937"] = {
-        TagText = "CO OWNER",
-        TagColor = Color3.fromRGB(199, 135, 17)
-    }
 }
