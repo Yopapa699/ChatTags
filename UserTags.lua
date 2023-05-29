538efc855d9109a19a57668ed2b0af466d46ee0d
Yxu_Sin <104718666+Yopapa699@users.noreply.github.com>
2023-05-29 20:21:18 +0800

    Update UserTags.lua

--- UserTags.lua
+++ UserTags.lua
@@ -1,10 +1,10 @@
 return {
     ["RifqiKil"] = {
         TagText = "OWNER",
-        TagColor = Color3.fromRGB(231, 76, 60)
+        TagColor = Color3.fromRGB(231,76,60)
     },
     ["NoJungleKiller"] = {
         TagText = "Private User",
-        TagColor = Color3.fromRGB(33, 203, 246)
+        TagColor = Color3.fromRGB(33,203,246)
     },
 }
